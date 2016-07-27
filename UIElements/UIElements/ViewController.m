215dc2d7fa73b46917097c14f04db9946b016ef8
Gerald Kim Ico <gerald_ico@launchlabs.ph>
2016-07-27 11:10:09 +0800

    ver 2.0

--- UIElements/UIElements/ViewController.m
+++ UIElements/UIElements/ViewController.m
@@ -16,8 +16,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
     //The custom method to create our textfield is called
     [self addTextField];
+    
+    //The custom method to create textfield with different keyboard input
+    [self addTextFieldWithDifferentKeyboard];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -37,7 +42,7 @@
     // This fits the frame to size of the text
     [prefixLabel sizeToFit];
     // This allocates the textfield and sets its frame
-    UITextField *textField = [[UITextField alloc] initWithFrame: CGRectMake(20, 50, 280, 30)];
+    UITextField *textField = [[UITextField alloc] initWithFrame: CGRectMake(20, 20, 150, 30)];
     // This sets the border style of the text field
     textField.borderStyle = UITextBorderStyleRoundedRect; textField.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
     [textField setFont:[UIFont boldSystemFontOfSize:12]];
@@ -51,6 +56,44 @@
     [self.view addSubview:textField];
     // sets the delegate to the current class
     textField.delegate = self;
+}
+
+-(void) addTextFieldWithDifferentKeyboard {
+    UITextField *textField1= [[UITextField alloc]initWithFrame: CGRectMake(20, 50, 280, 30)];
+    textField1.delegate = self;
+    textField1.borderStyle = UITextBorderStyleRoundedRect;
+    textField1.placeholder = @"Default Keyboard";
+    [self.view addSubview:textField1];
+    UITextField *textField2 = [[UITextField alloc]initWithFrame: CGRectMake(20, 100, 280, 30)];
+    textField2.delegate = self;
+    textField2.borderStyle = UITextBorderStyleRoundedRect;
+    textField2.keyboardType = UIKeyboardTypeASCIICapable;
+    textField2.placeholder = @"ASCII keyboard";
+    [self.view addSubview:textField2];
+    UITextField *textField3 = [[UITextField alloc]initWithFrame: CGRectMake(20, 150, 280, 30)];
+    textField3.delegate = self;
+    textField3.borderStyle = UITextBorderStyleRoundedRect;
+    textField3.keyboardType = UIKeyboardTypePhonePad;
+    textField3.placeholder = @"Phone pad keyboard";
+    [self.view addSubview:textField3];
+    UITextField *textField4 = [[UITextField alloc]initWithFrame: CGRectMake(20, 200, 280, 30)];
+    textField4.delegate = self;
+    textField4.borderStyle = UITextBorderStyleRoundedRect;
+    textField4.keyboardType = UIKeyboardTypeDecimalPad;
+    textField4.placeholder = @"Decimal pad keyboard";
+    [self.view addSubview:textField4];
+    UITextField *textField5= [[UITextField alloc]initWithFrame: CGRectMake(20, 250, 280, 30)];
+    textField5.delegate = self;
+    textField5.borderStyle = UITextBorderStyleRoundedRect;
+    textField5.keyboardType = UIKeyboardTypeEmailAddress;
+    textField5.placeholder = @"Email keyboard";
+    [self.view addSubview:textField5];
+    UITextField *textField6= [[UITextField alloc]initWithFrame: CGRectMake(20, 300, 280, 30)];
+    textField6.delegate = self;
+    textField6.borderStyle = UITextBorderStyleRoundedRect;
+    textField6.keyboardType = UIKeyboardTypeURL;
+    textField6.placeholder = @"URL keyboard";
+    [self.view addSubview:textField6];
 }
 
 // pragma mark is used for easy access of code in Xcode
